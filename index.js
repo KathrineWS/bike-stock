af251e25351fb7906507703712c011ba897e781c
Kathrine <kath415a@stud.kea.dk>
2022-10-14 10:12:39 +0200

    new branch incoming

---
+++ index.js
@@ -0,0 +1,15 @@
+// async function getData() {
+//     let result = await fetch (endpoint);
+//     showPosts(await result.json());
+// }
+
+postMessage.forEach((posts) => {
+  console.log(hello, post);
+
+  //template
+  const clone = template.cloneNode(true);
+  "template".content;
+  //clone
+  //put the content into it
+  //append it to the dom
+});
